621359698ef44a467997648f40b39f00f7dd7032
Addam Suarez <suarezaddam@gmail.com>
2026-02-14 18:02:41 -0600

    feat: migrate settings tab to base standalone screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { SettingsTabPlaceholderComponent } from './tabs/placeholders/settings-tab-placeholder.component';
 import { SummaryComponent } from './summary/summary.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { TodayComponent } from './today/today.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
   {
@@ -23,6 +23,6 @@ export const routes: Routes = [
   {
     path: 'tab-settings',
     outlet: 'settingsTab',
-    component: SettingsTabPlaceholderComponent,
+    component: SettingsComponent,
   },
 ];
