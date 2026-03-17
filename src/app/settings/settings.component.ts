10d2a7b9cf825f249fd2418f5d4a116b5a33dd0d
Addam Suarez <suarezaddam@gmail.com>
2026-03-16 20:31:36 -0500

    Update settings terms link

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -110,7 +110,8 @@ export class SettingsComponent implements OnInit, OnDestroy {
   }
 
   public openTermsOfService(): void {
-    Utils.openUrl('https://cctec.aspiware.com/terms-of-service');
+    // Utils.openUrl('https://cctec.aspiware.com/terms-of-service');
+    Utils.openUrl('https://www.apple.com/legal/internet-services/itunes/dev/stdeula/');
   }
 
   public logout(): void {
